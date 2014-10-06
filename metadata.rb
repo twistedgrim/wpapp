b274aed589e2277f2c0b5e26e41b5908ee1a1c2b
twistedgrim <rebel623@gmail.com>
2014-10-06 16:07:29 -0500

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# Encoding: utf-8
+name             'wpapp'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'none'
+license          ''
+description      'Installs/Configures wpapp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
