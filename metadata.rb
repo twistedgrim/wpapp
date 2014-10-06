ec7c5fd7112cd29cb75c0f23c6e80f0ab1f21a71
twistedgrim <rebel623@gmail.com>
2014-10-06 16:16:31 -0500

    Update Dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,7 @@ license          ''
 description      'Installs/Configures wpapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends  'apt'
+depends  'platformstack'
+depends  'rackops_rolebook'
+depends  'phpstack'
