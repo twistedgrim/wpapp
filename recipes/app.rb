ea035bb936555b566f5262e134dee4c198820f7e
twistedgrim <rebel623@gmail.com>
2014-10-07 09:52:24 -0500

    fix app.rb

--- recipes/app.rb
+++ recipes/app.rb
@@ -1,14 +1,18 @@
 node.default['apache']['sites']['example.com']['repository'] = 'https://github.com/mattjbarlow/wordpress_skeleton.git'
-node.default['apache']['sites']['example.com']['revision'] =  "HEAD"
+node.default['apache']['sites']['example.com']['revision'] = "HEAD"
 
-%w( phpstack::application_php
-),each do |recipe|
+%w( platformstack::default
+    rackops_rolebook::default
+    phpstack::application_php
+    phpstack::varnish
+  ).each do |recipe|
   include_recipe recipe
 end
 
 template "/var/www/example.com/current/wp-config.php" do
-  source "wp-config.php.erb
+  source "wp-config.php.erb"
   mode 0644
   owner "nobody"
   group "nogroup"
-end 
+end
+
