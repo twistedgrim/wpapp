dec3e93f07d16f65de54661f369c3151d2374b2f
twistedgrim <rebel623@gmail.com>
2014-10-08 13:02:26 -0500

    fixed directory issue

--- recipes/app.rb
+++ recipes/app.rb
@@ -9,10 +9,28 @@ node.default['apache']['sites']['example.com']['revision'] = "HEAD"
   include_recipe recipe
 end
 
-template "/var/www/example.com/current/wp-config.php" do
-  source "wp-config.php.erb"
-  mode 0644
-  owner "nobody"
-  group "nogroup"
+user "nobody" do
+  supports :manage_home => true
+  password "goodpassword"
 end
 
+group 'nogroup' do
+  action :create
+  members 'nobody'
+  append true
+end
+
+directory '/var/www/example.com/current/' do
+  owner 'nobody'
+  group 'nogroup'
+  mode '0644'
+  recursive true
+  action :create
+end
+
+template '/var/www/example.com/current/wp-config.php' do
+  source 'wp-config.php.erb'
+  mode 0644
+  owner 'nobody'
+  group 'nogroup'
+end
